9163e4b529fc992db32305286c55541018dc845d
Jere <Pozzee21@gmail.com>
2019-10-23 08:42:40 -0300

    a

--- app/src/main/java/com/s21/quemepongo2/ui/home/HomeViewModel.java
+++ app/src/main/java/com/s21/quemepongo2/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Esta es la pantalla principal ");
     }
 
     public LiveData<String> getText() {
